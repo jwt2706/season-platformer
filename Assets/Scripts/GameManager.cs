4f8cac32bd3d1d366b80eb82ff63200c0be21624
jwt2706 <jwt2home@gmail.com>
2025-06-22 22:46:33 -0400

    charms respawn after max reached

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 public enum Season
 {
@@ -12,12 +14,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    [Header("Charm Settings")]
+    public GameObject charmPrefab;
+    public Tilemap baseTilemap;
+    public int maxCharms = 3;
     public int charmsCollected = 0;
 
-    public Season currentSeason = Season.Spring; // Default to Spring
+    [Header("Season Settings")]
+    public Season currentSeason = Season.Spring;
 
     void Awake()
     {
+        // Singleton pattern
         if (Instance == null)
         {
             Instance = this;
@@ -29,23 +37,77 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        SpawnCharms();
+    }
+
     public void AddCharm()
     {
         charmsCollected++;
-        NextSeason();
-        Debug.Log("Charms Collected: " + charmsCollected);
+        Debug.Log("Charms Collected: " + charmsCollected + "/" + maxCharms);
+
+        if (charmsCollected >= maxCharms)
+        {
+            charmsCollected = 0;
+            NextSeason();
+            SpawnCharms();
+        }
     }
 
     public void NextSeason()
     {
         currentSeason = (Season)(((int)currentSeason + 1) % 4);
         Debug.Log("Season changed to: " + currentSeason);
-        // Optional: Trigger changes (e.g., update visuals, music)
+        // You can trigger season-based visuals or logic here
     }
 
     public void SetSeason(Season newSeason)
     {
         currentSeason = newSeason;
         Debug.Log("Season manually set to: " + currentSeason);
+    }
+
+    private void SpawnCharms()
+    {
+        List<Vector3Int> groundTiles = new List<Vector3Int>();
+
+        BoundsInt bounds = baseTilemap.cellBounds;
+        for (int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            for (int y = bounds.yMin; y < bounds.yMax; y++)
+            {
+                Vector3Int tilePos = new Vector3Int(x, y, 0);
+                if (baseTilemap.HasTile(tilePos))
+                {
+                    groundTiles.Add(tilePos);
+                }
+            }
+        }
+
+        Shuffle(groundTiles);
+
+        int spawned = 0;
+        foreach (var tilePos in groundTiles)
+        {
+            if (spawned >= maxCharms) break;
+
+            Vector3 worldPos = baseTilemap.CellToWorld(tilePos + Vector3Int.up);
+            worldPos += new Vector3(0.5f, 0.5f, 0); // center charm
+
+            Instantiate(charmPrefab, worldPos, Quaternion.identity);
+            spawned++;
+        }
+    }
+
+    private void Shuffle<T>(List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int rand = Random.Range(i, list.Count);
+            T temp = list[i];
+            list[i] = list[rand];
+            list[rand] = temp;
+        }
     }
 }
